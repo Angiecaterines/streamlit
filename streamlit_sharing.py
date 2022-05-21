802eddb0dabdf58f3559191f96d6548e278e34cb
Angiecaterines <66749378+Angiecaterines@users.noreply.github.com>
2022-05-21 12:21:17 -0500

    Add files via upload

--- streamlit_sharing.py
+++ streamlit_sharing.py
@@ -7,7 +7,7 @@ Created on Sat May 21 09:02:40 2022
 
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+
 
 
 st.title("My First Streamlit Web App")
@@ -15,12 +15,3 @@ st.title("My First Streamlit Web App")
 df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
 st.write(df)
 
-st.title("Gráfico")
-
-data_canada = px.data.gapminder().query("country == 'Canada'")
-px.line(data_canada, x='year', y='pop')
-
-st.title("Gráfico2")
-
-df1 = px.data.stocks()
-fig = px.line(df1, x='date', y="GOOG")
