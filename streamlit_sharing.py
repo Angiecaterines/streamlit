93753b0c9b15712c1ed33904835afbc3b0fd3381
Angiecaterines <66749378+Angiecaterines@users.noreply.github.com>
2022-05-21 12:17:42 -0500

    Add files via upload

--- streamlit_sharing.py
+++ streamlit_sharing.py
@@ -7,16 +7,15 @@ Created on Sat May 21 09:02:40 2022
 
 import streamlit as st
 import pandas as pd
-#from gsheetsdb import connect
+import plotly.express as px
 
 st.title("My First Streamlit Web App")
 
 df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
 st.write(df)
 
-#st.title("Connect to Google Sheets")
-#gsheet_url = "https://docs.google.com/spreadsheets/d/1_ccm9z_hr0o_Q6qpgE1PfgWnwlfTTMmElTKd92ZP09E/edit?usp=sharing"
-#conn = connect()
-#rows = conn.execute(f'SELECT * FROM "{gsheet_url}"')
-#df_gsheet = pd.DataFrame(rows)
-#st.write(df_gsheet)
+st.title("Gráfico")
+
+data_canada = px.data.gapminder().query("country == 'Canada'")
+px.line(data_canada, x='year', y='pop')
+
